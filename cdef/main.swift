2c82eeeb9de7ff6dd45e13a89f343ac5ccae03a9
François ftiff Levaux-Tiffreau <fti@me.com>
2016-02-17 09:42:13 +0100

    Initial Commit

---
+++ cdef/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  cdef
+//
+//  Created by François on 17/02/16.
+//  Copyright © 2016 François Levaux. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
